86318341f9cca78aa41c2fd95796b96b95d38372
Chanuth10 <chanuthmaduka1986@gmail.com>
2024-01-15 12:00:00 +0530

    order service place order function create

---
+++ order-service/src/main/java/com/chanuth/microservices/order/service/OrderService.java
@@ -0,0 +1,30 @@
+package com.chanuth.microservices.order.service;
+
+import com.chanuth.microservices.order.dto.OrderRequest;
+import com.chanuth.microservices.order.model.Order;
+import com.chanuth.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+        orderRepository.save(order);
+
+    }
+
+}
