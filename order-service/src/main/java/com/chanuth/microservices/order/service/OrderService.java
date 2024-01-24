be674ca11a716a2bd3e196bfc86f60cd7a99ef5b
Chanuth10 <chanuthmaduka1986@gmail.com>
2024-01-24 12:00:00 +0530

    check inventory service check stock function

--- order-service/src/main/java/com/chanuth/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/chanuth/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.chanuth.microservices.order.service;
 
+import com.chanuth.microservices.order.client.InventoryClient;
 import com.chanuth.microservices.order.dto.OrderRequest;
 import com.chanuth.microservices.order.model.Order;
 import com.chanuth.microservices.order.repository.OrderRepository;
@@ -16,14 +17,23 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
 
     }
 
